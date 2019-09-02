08c4f4ae2240f6c4e2cb15a3ba70bb7ceeaf3115
gunt <4261629+gunt@users.noreply.github.com>
2019-09-01 18:28:38 -0600

    wherever we type in getting logged down in console// testing functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
  // Arrow function, just to keep it ES6
 $(document).ready(() => {
-    alert(1);
+    //alert(1);
+
+    $('#searchForm').on('submit', (e) => {
+        console.log($('#searchText').val());
+        e.preventDefault();
+    });
+
 });
