ad946f2415c21f4caad8fbf6d19e8c1dd24c5ad8
gunt <4261629+gunt@users.noreply.github.com>
2019-09-02 15:54:49 -0600

    axios api update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
  // Arrow function, just to keep it ES6
 $(document).ready(() => {
     //alert(1);
-
     $('#searchForm').on('submit', (e) => {
-        console.log($('#searchText').val());
+        let searchText = $('#searchText').val();
+        // console.log($('#searchText').val());
+        getMovies(searchText);
         e.preventDefault();
     });
-
 });
+
+
+function getMovies(searchText){
+    // request to the axios Api
+
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US&query=' + searchText)
+    // axios.get('http://wwww.omdbapi.com?s=' + searchText)
+    //this is going to return a promise
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    
+}
