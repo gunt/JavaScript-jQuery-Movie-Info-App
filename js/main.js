0b4127f2ca4930a9d644cf22a0e519baec6fe912
gunt <4261629+gunt@users.noreply.github.com>
2019-09-02 19:46:15 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,3 +45,43 @@ function movieSelected(id){
     window.location = 'movie.html';
     return false;
 }
+
+function getMovie(){
+    let movieId = sessionStorage.getItem('movieId');
+    // Make a request for a user with a given ID
+    axios.get("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=98325a9d3ed3ec225e41ccc4d360c817")
+      .then(function (response) {
+      let movie = response.data;
+      //console.log(movie);
+      let output = `
+          <div class="row">
+            <div class="col-md-4">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="thumbnail">
+            </div>
+            <div class="col-md-8">
+              <h2>${movie.title}</h2>
+              <ul class="list-group">
+                <li class="list-group-item"><strong>Genre:</strong> ${movie.genres[0].name}, ${movie.genres[1].name}</li>
+                <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
+                <li class="list-group-item"><strong>Rated:</strong> ${movie.vote_average}</li>
+                <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime} min.</li>
+                <li class="list-group-item"><strong>Production Companies:</strong> ${movie.production_companies[0].name} min.</li>
+              </ul>
+            </div>
+          </div>
+          <div class="row">
+            <div class="well">
+              <h3>Plot</h3>
+              ${movie.overview}
+              <hr>
+              <a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-primary">View IMDB</a>
+              <a href="index.html" class="btn btn-default">Go Back To Search</a>
+            </div>
+          </div>
+      `;
+      $('#movie').html(output);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
